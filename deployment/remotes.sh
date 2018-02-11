69d400c3a2a5244de87939ba66cfa652ac889e03
jgallavin <jasong@lougeek.com>
2018-02-11 09:02:10 -0500

    add production environment, build config, and remote information.

---
+++ deployment/remotes.sh
@@ -0,0 +1,1 @@
+git remote add production ssh://www-data@159.89.189.187.com/var/repo/frontend.git
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUrl: 'http://159.89.189.187/api/'
 };
